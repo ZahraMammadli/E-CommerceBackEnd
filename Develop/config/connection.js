6069b0ed1c56919ded62bb66ad7af13a4a6d40b4
Zahra Mammadli <zahra.mrj@gmail.com>
2021-12-21 20:34:21 +0100

    initial commit

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -1,12 +1,13 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
+      host: "localhost",
+      // port: "3001",
+      dialect: "mysql",
       dialectOptions: {
         decimalNumbers: true,
       },
